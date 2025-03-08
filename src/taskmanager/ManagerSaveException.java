894258404e92fc1b5b32bbb67ede797f4aa4af92
GoldenWhey <shpagin.al@mail.ru>
2025-03-08 23:46:37 +0300

    Исправить код стайл

--- src/taskmanager/ManagerSaveException.java
+++ src/taskmanager/ManagerSaveException.java
@@ -1,9 +1,7 @@
 package taskmanager;
 
-import java.io.IOException;
-
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException (String message) {
+    public ManagerSaveException(String message) {
         super(message);
     }
 }
--- src/taskmanager/Managers.java
+++ src/taskmanager/Managers.java
@@ -6,6 +6,7 @@ public class Managers {
     public static TaskManager getDefault() {
         return new FileBackedTaskManager(new File("C:\\Users\\Aleksandr\\IdeaProjects\\java-kanban\\task.csv"));
     }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
